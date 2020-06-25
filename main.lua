ca94390d0cf257b35e6758e5ab5d801d245a35b4
Bavid Lynx <shivakharbanda11@gmail.com>
2020-06-25 15:12:22 +0530

    version update added static graphics

--- main.lua
+++ main.lua
@@ -9,6 +9,10 @@ push = require 'push'
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    smallFont = love.graphics.newFont('font.TTF', 8)
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         vsync = true,
@@ -24,10 +28,17 @@ end
 
 function love.draw()
     push:apply('start')
+    
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 5, 5)
+
+    love.graphics.rectangle('fill', 10, 35, 2, 25)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 45, 2, 25)
 
     love.graphics.printf("hello pong" ,
      0,
-     VIRTUAL_HEIGHT / 2 - 6,
+     20,
      VIRTUAL_WIDTH,
      'center')
 
